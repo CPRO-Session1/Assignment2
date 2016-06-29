b10e1369d81213f153b66e4730c8ad8491a790c7
Lloyd Page <lloyd.page@gmail.com>
2016-06-29 16:13:29 -0400

    commit randum.c

---
+++ randnum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	srand(time(0));
+	int r=rand();
+	r=r%10+1;
+	int g;
+	int x=1;
+	while(x)
+	{
+		printf("Enter your guess\n");
+		scanf("%d",&g);
+		if(r<g)
+		{
+			printf("you guessed too high\n");
+		}
+		else
+		{
+			if(r>g)
+				printf("you guess too low\n");
+			else
+			{
+				x=0;
+				printf("you guessed correctly\n");
+		}	}
+	}
+}
